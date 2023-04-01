ab3e57e70ba12cb40222beb04ef15f901bd3bf27
Chen Ping <fiyar@tom.com>
2023-04-01 18:29:08 +0800

    add file

---
+++ pytorch-test.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+data = torch.tensor([0.5, 0.2])
+w = torch.tensor([[1.0,2],[3,4]], requires_grad=True)
+m = torch.matmul(data, w)
+r = torch.nn.functional.softmax(torch.relu(m))
+print(r)
+label = torch.tensor([1, 0.0])
+d = r - label
+d = torch.mean(torch.mul(d, d))
+d.backward()
+print(d)
+print(w.grad)
+
+
+input_size = 28
+sequence_length = 28
+hidden_size = 128
+num_layers = 2
+num_classes = 10
+batch_size = 100
+num_epochs = 5
+learning_rate = 0.001
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
+test_dataset = datasets.MNIST(root='./data', train=False, transform=transform)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(LSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+        
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        
+        out, _ = self.lstm(x, (h0, c0))
+        
+        out = self.fc(out[:, -1, :])
+        
+        return out
+        
+device = torch.device('cpu') #torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = LSTM(input_size, hidden_size, num_layers, num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+        
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+        
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        
+    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
