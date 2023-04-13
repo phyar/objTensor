30ede159cfbd912991f6368a02821f53ac5cc53d
Chen Ping <fiyar@tom.com>
2023-04-13 18:09:29 +0800

    compare

--- pytorch-test.py
+++ pytorch-test.py
@@ -17,10 +17,10 @@ print(d)
 print(w.grad)
 
 
-input_size = 28
-sequence_length = 28
+sequence_length = 2
+input_size = 28*28//sequence_length
 hidden_size = 128
-num_layers = 2
+num_layers = 1
 num_classes = 10
 batch_size = 100
 num_epochs = 5
@@ -89,3 +89,5 @@ with torch.no_grad():
         correct += (predicted == labels).sum().item()
         
     print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+
+#torch.save(model, "my_model.pt")
